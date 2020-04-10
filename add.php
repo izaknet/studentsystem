1f5447c7eb63a2ef2cbcea04abd7bd042992c2ae
Nhap Argonza <nhap.argonza@Personiv.com>
2020-04-10 23:09:55 +0800

    First Commit

---
+++ add.php
@@ -0,0 +1,56 @@
+<?php
+
+    include_once("connections/connection.php");
+
+    $con = connection();
+
+    if(isset($_POST['submit'])){
+
+        $fname = $_POST['firstname'];
+        $lname = $_POST['lastname'];
+        $gender = $_POST['gender'];
+
+        $sql = "INSERT INTO `student_list`(`first_name`, `last_name`, `gender`) VALUES ('$fname','$lname','$gender')";
+        $con->query($sql) or die ($con->error);
+
+        echo header("Location: index.php");
+
+    }
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Student Management System</title>
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+    <body>
+     
+
+        <form action="" method="post">
+            
+            <label>First Name</label>
+            <input type="text" name="firstname" id="fname">
+
+            <label>Last Name</label>
+            <input type="text" name="lastname" id="lname">
+
+            <label>Gender</label>
+            <select name="gender" id="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
+
+            <input type="submit" name="submit" value="Submit Form">
+
+        </form>
+
+            
+
+    </body>
+    </html>
+    
